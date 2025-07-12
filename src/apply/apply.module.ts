6612c58b7d7b257ad2a256bdcc6b0ee6edd3d7b4
hyun071111 <jisaki07@gmail.com>
2025-07-13 02:06:23 +0900

    feat: 지원현황 페이지 기능 연결

--- src/apply/apply.module.ts
+++ src/apply/apply.module.ts
@@ -5,6 +5,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserEntity } from 'src/user/entities/user.entity';
 import { UserModule } from 'src/user/user.module';
 import { JobInformationEntity } from 'src/job/entities/job-information.entity';
+import { CompanyInformationEntity } from 'src/job/entities/company-information.entity';
 import { ApplicationFileEntity } from './entities/application-file.entity';
 import { ApplicationStatusEntity } from './entities/application-status.entity';
 import { JwtAuthGuard } from 'src/auth/guard/jwt.guard';
@@ -18,6 +19,7 @@ import { AuthModule } from 'src/auth/auth.module';
       ApplicationStatusEntity,
       UserEntity,
       JobInformationEntity,
+      CompanyInformationEntity,
     ]),
     AuthModule,
   ],
