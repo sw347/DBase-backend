787870d2955964dd1e5c8c0376c52faf58231ffb
sw347 <sdh230310@sdh.hs.kr>
2025-07-25 11:45:56 +0900

    chore: ip와 domain 추가

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,13 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: [
+      'http://localhost:5173',
+      'http://dbase.o-r.kr/',
+      'http://15.164.251.148/',
+    ],
     credentials: true,
-  })
+  });
   await app.listen(process.env.PORT ?? 4433);
 }
 
