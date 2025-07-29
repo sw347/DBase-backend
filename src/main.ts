0d4261420e32c89915b216f2c25fcab48cf42fe5
sw347 <sdh230310@sdh.hs.kr>
2025-07-29 15:36:57 +0900

    fix: cors error 해결 대처

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
   app.enableCors({
-    origin: ['https://dbase.o-r.kr'],
-    credentials: true,
+    origin: 'https://dbase.o-r.kr',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    callowedHeaders: 'Content-Type, Accept, Authorization',
+    redentials: true,
   });
   await app.listen(process.env.PORT ?? 3344);
 }
