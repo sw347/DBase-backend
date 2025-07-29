521b583e55ae91f9cf0f4b1969e4e5eb108fa480
sw347 <sdh230310@sdh.hs.kr>
2025-07-29 13:49:21 +0900

    chore: cors origin

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
   app.enableCors({
-    origin: '*',
+    origin: ['https://dbase.o-r.kr'],
     credentials: true,
   });
   await app.listen(process.env.PORT ?? 3344);
